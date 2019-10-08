e1027f86d9784098e8d2f96ce1b006c597db022f
ajaidanial <ajaidanial@gmail.com>
2019-10-08 05:34:11 +0530

    signup | 2

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -3,148 +3,16 @@ import { Link as RouterLink, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import validate from 'validate.js'
 import { makeStyles } from '@material-ui/styles'
-import {
-  Grid,
-  Button,
-  IconButton,
-  TextField,
-  Link,
-  FormHelperText,
-  Checkbox,
-  Typography
-} from '@material-ui/core'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import { Grid, Button, TextField, Link, Typography } from '@material-ui/core'
 
-const schema = {
-  firstName: {
-    presence: { allowEmpty: false, message: 'is required' },
-    length: {
-      maximum: 32
-    }
-  },
-  lastName: {
-    presence: { allowEmpty: false, message: 'is required' },
-    length: {
-      maximum: 32
-    }
-  },
-  email: {
-    presence: { allowEmpty: false, message: 'is required' },
-    email: true,
-    length: {
-      maximum: 64
-    }
-  },
-  password: {
-    presence: { allowEmpty: false, message: 'is required' },
-    length: {
-      maximum: 128
-    }
-  },
-  policy: {
-    presence: { allowEmpty: false, message: 'is required' },
-    checked: true
-  }
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.default,
-    height: '100%'
-  },
-  grid: {
-    height: '100%'
-  },
-  quoteContainer: {
-    [theme.breakpoints.down('md')]: {
-      display: 'none'
-    }
-  },
-  quote: {
-    backgroundColor: theme.palette.neutral,
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundImage: 'url(/images/auth.jpg)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center'
-  },
-  quoteInner: {
-    textAlign: 'center',
-    flexBasis: '600px'
-  },
-  quoteText: {
-    color: theme.palette.white,
-    fontWeight: 300
-  },
-  name: {
-    marginTop: theme.spacing(3),
-    color: theme.palette.white
-  },
-  bio: {
-    color: theme.palette.white
-  },
-  contentContainer: {},
-  content: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  contentHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingTop: theme.spacing(5),
-    paddingBototm: theme.spacing(2),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2)
-  },
-  logoImage: {
-    marginLeft: theme.spacing(4)
-  },
-  contentBody: {
-    flexGrow: 1,
-    display: 'flex',
-    alignItems: 'center',
-    [theme.breakpoints.down('md')]: {
-      justifyContent: 'center'
-    }
-  },
-  form: {
-    paddingLeft: 100,
-    paddingRight: 100,
-    paddingBottom: 125,
-    flexBasis: 700,
-    [theme.breakpoints.down('sm')]: {
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(2)
-    }
-  },
-  title: {
-    marginTop: theme.spacing(3)
-  },
-  textField: {
-    marginTop: theme.spacing(2)
-  },
-  policy: {
-    marginTop: theme.spacing(1),
-    display: 'flex',
-    alignItems: 'center'
-  },
-  policyCheckbox: {
-    marginLeft: '-14px'
-  },
-  signUpButton: {
-    margin: theme.spacing(2, 0)
-  }
-}))
+import { schema } from './schema' // schema for the form data
+import { styles } from './styles' // styles for the components
 
 const SignUp = (props) => {
   const { history } = props
-
-  const classes = useStyles()
-
+  // for styling the components
+  const classes = makeStyles(styles)()
+  // the state for the whole page
   const [formState, setFormState] = useState({
     isValid: false,
     values: {},
@@ -152,9 +20,12 @@ const SignUp = (props) => {
     errors: {}
   })
 
+  /**
+   * Validates the given data against the `schema`.
+   * Sets the errors in the state.
+   */
   useEffect(() => {
     const errors = validate(formState.values, schema)
-
     setFormState((formState) => ({
       ...formState,
       isValid: errors ? false : true,
@@ -162,17 +33,18 @@ const SignUp = (props) => {
     }))
   }, [formState.values])
 
+  /**
+   * Called on change of values in input fields.
+   * Stores values to the state.
+   * @param {form change event} event
+   */
   const handleChange = (event) => {
     event.persist()
-
     setFormState((formState) => ({
       ...formState,
       values: {
         ...formState.values,
-        [event.target.name]:
-          event.target.type === 'checkbox'
-            ? event.target.checked
-            : event.target.value
+        [event.target.name]: event.target.value
       },
       touched: {
         ...formState.touched,
@@ -181,53 +53,31 @@ const SignUp = (props) => {
     }))
   }
 
-  const handleBack = () => {
-    history.goBack()
-  }
-
+  /**
+   * Handles the successful form submit.
+   * @param {form submit event} event
+   */
   const handleSignUp = (event) => {
     event.preventDefault()
     history.push('/')
   }
 
+  /**
+   * Returns a bool if the input field has errors
+   * @param {`name` of input} field
+   */
   const hasError = (field) =>
     formState.touched[field] && formState.errors[field] ? true : false
 
   return (
     <div className={classes.root}>
       <Grid className={classes.grid} container>
-        <Grid className={classes.quoteContainer} item lg={5}>
-          <div className={classes.quote}>
-            <div className={classes.quoteInner}>
-              <Typography className={classes.quoteText} variant="h1">
-                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                they sold out High Life.
-              </Typography>
-              <div className={classes.person}>
-                <Typography className={classes.name} variant="body1">
-                  Takamaru Ayako
-                </Typography>
-                <Typography className={classes.bio} variant="body2">
-                  Manager at inVision
-                </Typography>
-              </div>
-            </div>
-          </div>
-        </Grid>
-        <Grid className={classes.content} item lg={7} xs={12}>
+        <Grid className={classes.content} item lg={12} xs={12}>
           <div className={classes.content}>
-            <div className={classes.contentHeader}>
-              <IconButton onClick={handleBack}>
-                <ArrowBackIcon />
-              </IconButton>
-            </div>
             <div className={classes.contentBody}>
               <form className={classes.form} onSubmit={handleSignUp}>
                 <Typography className={classes.title} variant="h2">
                   Create new account
-                </Typography>
-                <Typography color="textSecondary" gutterBottom>
-                  Use your email to create new account
                 </Typography>
                 <TextField
                   className={classes.textField}
@@ -285,36 +135,6 @@ const SignUp = (props) => {
                   value={formState.values.password || ''}
                   variant="outlined"
                 />
-                <div className={classes.policy}>
-                  <Checkbox
-                    checked={formState.values.policy || false}
-                    className={classes.policyCheckbox}
-                    color="primary"
-                    name="policy"
-                    onChange={handleChange}
-                  />
-                  <Typography
-                    className={classes.policyText}
-                    color="textSecondary"
-                    variant="body1"
-                  >
-                    I have read the{' '}
-                    <Link
-                      color="primary"
-                      component={RouterLink}
-                      to="#"
-                      underline="always"
-                      variant="h6"
-                    >
-                      Terms and Conditions
-                    </Link>
-                  </Typography>
-                </div>
-                {hasError('policy') && (
-                  <FormHelperText error>
-                    {formState.errors.policy[0]}
-                  </FormHelperText>
-                )}
                 <Button
                   className={classes.signUpButton}
                   color="primary"
@@ -327,7 +147,7 @@ const SignUp = (props) => {
                   Sign up now
                 </Button>
                 <Typography color="textSecondary" variant="body1">
-                  Have an account?{' '}
+                  Already have an account?{' '}
                   <Link component={RouterLink} to="/sign-in" variant="h6">
                     Sign in
                   </Link>
---
+++ src/views/SignUp/schema.js
@@ -0,0 +1,28 @@
+export const schema = {
+  firstName: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 32
+    }
+  },
+  lastName: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 32
+    }
+  },
+  email: {
+    presence: { allowEmpty: false, message: 'is required' },
+    email: true,
+    length: {
+      maximum: 64
+    }
+  },
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 128,
+      minimum: 8
+    }
+  }
+}
